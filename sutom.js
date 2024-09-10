c90447eb226842c24d80d91b528154fd16d116c5
Lorelei Wannyn <loreleiwannyn@MacBook-Air-de-Lorelei.local>
2024-09-10 14:21:36 +0200

    mot aleatoire

--- sutom.js
+++ sutom.js
@@ -1,10 +1,24 @@
 // création d'une liste de mots 
 let words = ["pomme", "banane", "cerise", "noix", "ananas"];
 
-// création d'une fonction pour générer un mot aléatoire
+console.log(words);
 
+// Création d'une fonction pour choisir un mot du tableau 
 
-function getRandomWord(words) {
-  return words[Math.floor(Math.random() * words.length)];
+function randomword(words) {
+   return words[Math.floor(Math.random() * words.length)];
 }
+
+// stocker le mot aléatoire dans une variable
+let random = randomword(words);
+console.log(random);
+
+
+
+// découper le mot en tableau et stocker chaque lettre dans une variable 
+
+
+let letters = random.split("");
+console.log(letters);
+
 
